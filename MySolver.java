1fadaf873496b057f500bc29f470bf0db121cd1e
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2023-10-11 10:11:23 +0100

    Create MySolver.java
    Add solver class

---
+++ MySolver.java
@@ -0,0 +1,14 @@
+/**
+ *  Write a backtracking algorithm to solve a sudoku board.
+ *
+ *  I.e. Fill in an empty cell with the first allowed value (i.e. 1) and if there is no violation of constraints, move
+ *  onto the next cell. If there is a violation, increment the value used. Once the value reaches 9 and there is a violation
+ *  backtrack to the previous cell.
+ */
+public class MySolver implements SudukoSolver {
+
+    @Override
+    public void solve(int[][] board) {
+
+    }
+}
